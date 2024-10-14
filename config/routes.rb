539dedc9214e4273ae4d3c4fed77ffe6a926bef4
EC2 Default User <ec2-user@ip-172-31-11-13.ap-northeast-1.compute.internal>
2024-10-14 03:27:46 +0000

    [Add]routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,44 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+
+  # ユーザー用
+  # URL /users/sign_in ...
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
   
-# 顧客用
-# URL /users/sign_in ...
-devise_for :users,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
-
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
-
-
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  # ユーザー側のルーティング設定
+  scope module: :public do
+    root to: "homes#top"
+    get 'about', to: 'homes#about', as: :about
+    
+    get "/mypage", to: "users#mypage", as: "mypage"
+    resources :users, only: [:edit,:show,:update,:destroy] do
+      resource :relationships, only: [:create, :destroy]
+        get "followings" => "relationships#followings", as: "followings"
+        get "followers" => "relationships#followers", as: "followers"
+    end
+    
+    resources :posts do
+      resource :comments, only: [:create, :destroy]
+      resources :favorite, only: [:create, :destroy]
+    end
+    
+    get "/search", to: "searches#search"
+  end
+  
+  # 管理者側のルーティング設定
+  namespace :admin do
+    get "", to: "homes#top"
+    
+    resources :users, only: [:show, :edit, :update]
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
