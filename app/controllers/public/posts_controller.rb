08dc61e5de8c81180c04af68ce92e5e76acc5ca9
EC2 Default User <ec2-user@ip-172-31-11-13.ap-northeast-1.compute.internal>
2024-10-17 09:01:20 +0000

    [Add]新規投稿フォーム（仮）作成

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,22 +1,45 @@
 class Public::PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
+    @user = @Post.user
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to post_path(@post)
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
+  
+  #ストロングパラメータ
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
+  end
+  
 end
